8b43096e402796356596611a88ba55997ae8e8e0
John Anderson <madroxprime@gmail.com>
2016-07-19 08:14:45 -0500

    TroubleShooting

--- WorkFlow.js
+++ WorkFlow.js
@@ -137,7 +137,7 @@ function restoreNote(note){
 	buttonClose.setAttribute('onClick',"CloseElement(this)")
 	buttonClose.innerHTML = 'Finish'
 	var deleteButton = document.createElement('button');
-	deleteButton.setAttribute('onClick',"console.log(this.getParent.getParent.id)");
+	deleteButton.setAttribute('onClick',"console.log(this)");
 	deleteButton.innerHTML = 'Delete'
 	header.appendChild(button);
 	header.appendChild(buttonClose);
