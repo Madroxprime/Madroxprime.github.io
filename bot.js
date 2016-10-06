c212addf884100a94306f6cb5befab205559a58a
Madroxprime <madroxprime@Gmail.com>
2016-10-05 20:46:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,5 +45,5 @@ switch (e.message.content){
 });
 
 client.Dispatcher.on("VOICE_CHANNEL_JOIN", e => {
-    e.message.channel.sendMessage("Welcome back, "+e.user.username);
+    e.channel.sendMessage("Welcome back, "+e.user.username);
 });
